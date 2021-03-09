de30c5ccd77866980b612a3ad10fcd1b4f500655
Evgeny Geyer <evgeny.geyer@ya.ru>
2021-03-09 20:27:27 +0300

    изменено расположение создание инстанса апи

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -32,12 +32,4 @@ class Api {
   }
 }
 
-const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-20",
-  headers: {
-    authorization: "79accf8f-cc76-4033-84f9-2d1d81c30157",
-    "Content-Type": "application/json",
-  },
-});
-
-export { Api, api };
+export default Api;
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,15 @@
+import Api from "./Api";
+
+const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-20",
+  headers: {
+    authorization: "79accf8f-cc76-4033-84f9-2d1d81c30157",
+    "Content-Type": "application/json",
+  },
+});
+
 const showErrorMassage = (err) => {
   console.error(err);
 };
 
-export { showErrorMassage };
+export { showErrorMassage, api };
