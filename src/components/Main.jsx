8b7d5491c97bbc39065fd35be5ef65fa8a7c2241
Евгений Гейер <evgeny.geyer@ya.ru>
2021-03-17 16:50:43 +0300

    импортирован реакт

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useContext } from "react";
 import Card from "./Card.jsx";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
