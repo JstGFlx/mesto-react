24b791529e9739c73782260fe73573e5dfee1948
Евгений Гейер <evgeny.geyer@ya.ru>
2021-03-23 18:29:26 +0300

    изменено название api

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
-import Api from "./Api";
+import Api from './api';
 
 const api = new Api({
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-20",
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-20',
   headers: {
-    authorization: "79accf8f-cc76-4033-84f9-2d1d81c30157",
-    "Content-Type": "application/json",
+    authorization: '79accf8f-cc76-4033-84f9-2d1d81c30157',
+    'Content-Type': 'application/json',
   },
 });
 
