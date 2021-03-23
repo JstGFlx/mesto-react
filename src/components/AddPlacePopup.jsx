f28ba3346b383567f68749263ab29a3d7c19deeb
Евгений Гейер <evgeny.geyer@ya.ru>
2021-03-23 16:24:49 +0300

    добавлена функция очистки полей

--- src/components/AddPlacePopup.jsx
+++ src/components/AddPlacePopup.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import PopupWithForm from "./PopupWithForm";
+import { useState, useEffect } from 'react';
+import PopupWithForm from './PopupWithForm';
 
 function AddPlacePopup(props) {
-  const [name, setName] = useState("");
-  const [link, setLink] = useState("");
+  const [name, setName] = useState('');
+  const [link, setLink] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,48 +15,53 @@ function AddPlacePopup(props) {
   };
 
   const handleChange = (e) => {
-    e.target.name === "name"
+    e.target.name === 'name'
       ? setName(e.target.value)
       : setLink(e.target.value);
   };
 
+  useEffect(() => {
+    setName('');
+    setLink('');
+  }, [props.isOpen]);
+
   return (
     <PopupWithForm
       key={1}
-      name="add"
-      title="Новое место"
+      name='add'
+      title='Новое место'
       isOpen={props.isOpen}
       onClose={props.onClose}
       onSubmit={handleSubmit}
     >
       <input
-        className="popup__input popup__input_text_title"
+        className='popup__input popup__input_text_title'
         value={name}
         onChange={handleChange}
-        name="name"
-        id="name-card"
-        type="text"
-        placeholder="Название"
-        minLength="2"
-        maxLength="30"
+        name='name'
+        id='name-card'
+        type='text'
+        placeholder='Название'
+        minLength='2'
+        maxLength='30'
         required
       />
-      <span className="popup__error" id="name-card-error" />
+      <span className='popup__error' id='name-card-error' />
       <input
-        className="popup__input popup__input_text_link"
+        className='popup__input popup__input_text_link'
         value={link}
         onChange={handleChange}
-        name="link"
-        id="url-card"
-        type="url"
-        placeholder="Ссылка на картинку"
+        name='link'
+        id='url-card'
+        type='url'
+        placeholder='Ссылка на картинку'
         required
       />
-      <span className="popup__error" id="url-card-error" />
+      <span className='popup__error' id='url-card-error' />
       <button
-        className="btn btn_margin_l popup__button popup__button_type_add"
-        type="submit"
-        aria-label="сохранить"
+        className='btn btn_margin_l popup__button popup__button_type_add'
+        type='submit'
+        aria-label='сохранить'
       >
         Создать
       </button>
