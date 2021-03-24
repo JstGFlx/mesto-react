45ac7f917e2712d0292abb08ac781c9f00ef6a45
Евгений Гейер <evgeny.geyer@ya.ru>
2021-03-24 14:06:57 +0300

    сделана валидация попапа автара

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,16 +1,30 @@
-import { useRef } from 'react';
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import PopupWithForm from './PopupWithForm';
 
 function EditAvatarPopup(props) {
-  const link = useRef(null);
+  const {
+    register,
+    handleSubmit,
+    errors,
+    reset,
+    getValues,
+    formState,
+    trigger,
+  } = useForm({
+    mode: 'all',
+  });
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const { isValid } = formState;
 
-    props.onUpdateAvatar({
-      avatar: link.current.value,
-    });
+  const onSubmit = () => {
+    props.onUpdateAvatar(getValues());
+    trigger();
   };
+
+  useEffect(() => {
+    reset();
+  }, [props.isOpen, reset]);
 
   return (
     <PopupWithForm
@@ -19,20 +33,34 @@ function EditAvatarPopup(props) {
       title='Обновить аватар'
       isOpen={props.isOpen}
       onClose={props.onClose}
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <input
         className='popup__input popup__input_text_link'
-        name='link'
+        name='avatar'
         type='url'
         placeholder='Ссылка на картинку'
-        ref={link}
-        required
+        ref={register({
+          required: true,
+          pattern: {
+            value: /^https?:\/\//,
+            message: 'Пожалуйста, введите URL в формате https:// ...',
+          },
+        })}
       />
+      {errors.link && errors.link.type === 'required' && (
+        <span className='popup__error'>Пожалуйста, заполните это поле.</span>
+      )}
+      {errors.link && (
+        <span className='popup__error'>{errors.link.message}</span>
+      )}
       <button
-        className='btn btn_margin_l popup__button popup__button_type_edit-avatar'
+        className={`btn btn_margin_l popup__button popup__button_type_add ${
+          !isValid && 'popup__button_disabled'
+        }`}
         type='submit'
         aria-label='сохранить'
+        disabled={!isValid}
       >
         Сохранить
       </button>
