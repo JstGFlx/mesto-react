24072b95fa00951673f6f9010aa51f7c0d97bfec
Евгений Гейер <evgeny.geyer@ya.ru>
2021-04-06 22:04:31 +0300

    фача: написана логика и стили выпадающего меню

--- src/components/BurgerMenuButton.js
+++ src/components/BurgerMenuButton.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export const BurgerMenuButton = () => {
-  //burger-menu_active
-
+export const BurgerMenuButton = (props) => {
   return (
-    <svg className={`burger-menu`} viewBox='0 0 100 100' width='55'>
+    <svg
+      className={`burger-menu ${props.isClicked ? 'burger-menu_active' : ''} ${
+        props.isLoggedIn ? '' : 'burger-menu_logged-in'
+      }`}
+      viewBox='0 0 100 100'
+      width='55'
+      onClick={props.onClick}
+    >
       <path
         className='burger-menu__line burger-menu__line_type_top'
         d='m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272'
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import logoPath from '../images/logo/Vector.svg';
 import { Link, useHistory } from 'react-router-dom';
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
@@ -7,14 +7,40 @@ import { BurgerMenuButton } from './BurgerMenuButton';
 function Header(props) {
   const history = useHistory();
   const currentUser = useContext(CurrentUserContext);
+  const [isMenuButtonOpen, setIsMenuButtonOpen] = useState(false);
+  useEffect(() => {
+    console.log(props.isLogined);
+  }, [props.isLogined]);
+
+  const handleMenuButtonClick = () => {
+    setIsMenuButtonOpen(!isMenuButtonOpen);
+  };
 
   return (
-    <header className='header'>
+    <header
+      className={`header ${
+        isMenuButtonOpen && props.isLogined ? 'header_active' : ''
+      } ${props.isLogined ? 'header_logged-in' : ''}`}
+    >
       <div className='header__container'>
-        <img className='header__logo' src={logoPath} alt='логотип место' />
-        <BurgerMenuButton />
+        <img
+          className={`header__logo ${
+            props.isLogined ? 'header__logo_logged-in' : ''
+          }`}
+          src={logoPath}
+          alt='логотип место'
+        />
+        <BurgerMenuButton
+          onClick={handleMenuButtonClick}
+          isClicked={isMenuButtonOpen}
+          isLoggedIn={props.isLogined}
+        />
       </div>
-      <div className='header__auth'>
+      <div
+        className={`header__auth ${
+          props.isLogined ? 'header__auth_logged-in ' : ''
+        }`}
+      >
         <p className='header__email'>{currentUser?.email}</p>
         <Link
           className={`header__link ${
