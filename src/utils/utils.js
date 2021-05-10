c71a2cf4d37d1dfe5d02b349d1c58b53dd7f326c
Evgeny Geyer <evgeny.geyer@ya.ru>
2021-05-10 22:19:34 +0300

    изменен базовый юрл

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ import Api from './api';
 import AuthApi from './AuthApi';
 
 export const api = new Api({
-  baseUrl: 'https://jstgflx.nomoredomains.club/',
+  baseUrl: 'https://api.jstgflx.nomoredomains.club/',
   headers: {
     'Content-Type': 'application/json',
   },
